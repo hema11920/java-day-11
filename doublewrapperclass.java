72d622711e98dae77058fa7bbd79f76f41e30943
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-12 14:50:53 +0530

    Add files via upload

---
+++ doublewrapperclass.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class doublewrapperclass{
+    public static void main(String args[]){
+        Double a = new Double(89);
+        System.out.println(a);
+    }
+}
---
+++ integerwrapperclass.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class integerwrapperclass{
+    public static void main(String args[]){
+        Integer a = new.nteger(90);
+        System.out.println(a);
+    }
+}
